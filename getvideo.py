99176526cb91ae1618c939e1f58bc0886dce3404
Matthew Thomas <matt.thomas@jacksonriver.com>
2020-07-23 12:10:33 -0400

    adding more presets

--- getvideo.py
+++ getvideo.py
@@ -3,6 +3,7 @@ import sys
 from bs4 import BeautifulSoup
 import re
 VIDEO_RES = [
+    'HLS_270',
     'DASH_9_6_M',
     'DASH_4_8_M',
     'DASH_2_4_M',
@@ -12,7 +13,17 @@ VIDEO_RES = [
     'DASH_720',
     'DASH_480',
     'DASH_360',
-    'DASH_240'
+    'DASH_240',
+    'DASH_9_6_M.mp4',
+    'DASH_4_8_M.mp4',
+    'DASH_2_4_M.mp4',
+    'DASH_1_2_M.mp4',
+    'DASH_600_K.mp4',
+    'DASH_1080.mp4',
+    'DASH_720.mp4',
+    'DASH_480.mp4',
+    'DASH_360.mp4',
+    'DASH_240.mp4'
     ]
 
 # load up the provided link
@@ -25,7 +36,6 @@ if (result.status_code == 200):
     videos = soup.find(attrs={"href": re.compile("https://v.redd.it")})
     if videos and len(videos) > 0:
         href = videos['href']
-        print(href)
         for res in VIDEO_RES:
             dash = href + '/' + res
             dashresult = requests.get(dash, allow_redirects=False, headers = {'User-agent': "badideabot 0.1"})
